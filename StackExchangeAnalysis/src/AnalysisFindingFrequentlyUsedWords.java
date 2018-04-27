3df69a9244e4a17a6e393dbc883634a8ff7063b5
Banjardar <banjardar@outlook.com>
2018-04-27 19:56:59 -0400

    Stack Exchange Analysis - Hadoop Experiments

---
+++ StackExchangeAnalysis/src/AnalysisFindingFrequentlyUsedWords.java
@@ -0,0 +1,61 @@
+
+//This is a generic map reduce outline class provided as part of the 
+//Virtual Pair Programmers Hadoop For Java Developers training course.
+//(www.virtualpairprogrammers.com). You may freely use this file for your
+//own development.
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.stackexcahnge.maps.SEMoreFrequentlyUsedWordsMapper;
+import org.stackexcahnge.reducers.StackExchangeMoreFrequentlyUsedWordsReducer;
+import org.stackexcahnge.util.SEUtility;
+
+
+public class AnalysisFindingFrequentlyUsedWords  {
+
+	public static void main(String[] args) throws Exception {
+        Path in = new Path(args[0]);
+        Path out = new Path(args[1]);
+        
+        Configuration conf = new Configuration();
+        SEUtility.deletePreviousOutput(conf, out);
+		//set any configuration params here. eg to say that the key and value are comma
+		//separated in the input data add:
+		//conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "&");
+
+		Job job = Job.getInstance(conf);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		
+		job.setMapperClass(SEMoreFrequentlyUsedWordsMapper.class);
+
+		//uncomment the following line if specifying a combiner
+		//job.setCombinerClass(Combine.class);
+		
+		System.out.println("*****AnalysisOfQuestionsByDate****main************************ ");
+		job.setReducerClass(StackExchangeMoreFrequentlyUsedWordsReducer.class);
+		
+		job.setInputFormatClass(KeyValueTextInputFormat.class); 
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, in);
+		FileOutputFormat.setOutputPath(job, out);
+		
+		job.setJarByClass(AnalysisFindingFrequentlyUsedWords.class);
+		job.submit();
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ StackExchangeAnalysis/src/org/stackexcahnge/maps/SEMoreFrequentlyUsedWordsMapper.java
@@ -0,0 +1,27 @@
+package org.stackexcahnge.maps;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.stackexcahnge.writable.SEWritable;
+
+public class SEMoreFrequentlyUsedWordsMapper extends Mapper<Text, Text,Text, LongWritable > {
+
+	public void map (Text key, Text value, Context context)
+			throws IOException, InterruptedException {
+		SEWritable seValue= new SEWritable(value.toString());
+		String text = seValue.getPostText().toLowerCase();
+		
+		String trimmedText = text.replaceAll("[^a-zA-Z\\s]", "");
+	
+		String[] words = trimmedText.split((" "));
+		
+		for (int i = 0; i < words.length -1; i++) {
+			if (words[i].length() >=4 && words[i+1].length() >=4) {
+				context.write(new Text(words[i]+" "+words[i+1]),new LongWritable(1));
+			}
+		}
+	}
+}
